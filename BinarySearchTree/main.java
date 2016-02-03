3356db468d9cbc85472d9716e8480fed22efc0fa
Amandeep Sarow <asarow@ucsd.edu>
2016-02-03 13:51:34 -0800

    Tree inversion implemented

--- BinarySearchTree/main.java
+++ BinarySearchTree/main.java
@@ -1,13 +1,21 @@
 import java.util.Random;
+
 public class main {
     public static void main(String[] args) {
 	BST<Integer> bst = new BST<Integer>();
 	Random random = new Random();
-	
-	for (int i = 0; i < 20; ++i) {
-	    bst.insert(random.nextInt(20));
+
+	for (int i = 0; i < 2; ++i) {
+	    bst.insert(random.nextInt(10));
 	}
 
-	bst.displayData();
+	bst.display();
+
+	System.out.println();
+	
+	bst.invert();
+
+	bst.display();
     }
+    
 }
