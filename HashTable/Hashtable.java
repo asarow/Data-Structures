aa1ee7e31e30e9fc500c1b21849c6220d7ba4521
Amandeep Sarow <asarow@ucsd.edu>
2016-02-04 14:12:11 -0800

    Basic implementation finished

---
+++ HashTable/Hashtable.java
@@ -0,0 +1,89 @@
+import java.util.LinkedList;
+import java.util.ArrayList;
+
+public class Hashtable <K,V> {
+    private ArrayList<Entry> entry;
+    private int capacity;
+    private int size;
+
+    @SuppressWarnings("unchecked")
+    Hashtable(int capacity) {
+	this.size = 0;
+	this.capacity = capacity;;
+	entry = new ArrayList<Entry>();
+	initializeTable();
+    }
+    
+    private class Entry {
+	private K key;
+	private V value;
+	private Entry next;
+	private Entry prev;
+	
+	Entry(K key, V value, Entry prev, Entry next) {
+	    this.key = key;
+	    this.value = value;
+	    this.prev = prev;
+	    this.next = next;
+	}
+
+	public void setNext(Entry next) {
+	    this.next = next;
+	}
+
+	public void setPrev(Entry prev) {
+	    this.prev = prev;
+	}
+
+	public Entry getNext() {
+	    return this.next;
+	}
+
+	public Entry getPrev() {
+	    return this.prev;
+	}
+	
+	public K getKey() {
+	    return this.key;
+	}
+
+	public V getValue() {
+	    return this.value;
+	}
+    }
+    
+    public void put(K key, V value) {
+	int index = hash(key);
+	Entry curr = entry.get(index);
+	while (curr.getNext() != null) {
+	    curr = curr.getNext();
+	}
+	Entry newEntry = new Entry(key, value, curr, null);
+	curr.setNext(newEntry);
+	size++;
+    }   
+
+    public double loadFactor() {
+	return (double) size / (double) capacity;
+    }
+    
+    
+    public void display() {
+	for (Entry x : entry) {
+	    while(x.getNext() != null) {     
+		x = x.getNext();
+		System.out.print("[" + x.getKey() + ", " +  x.getValue() + "] ");
+	    } System.out.println();
+	}
+    }
+
+    private int hash(K key) {
+	return  key.hashCode() % capacity;
+    }
+
+    private void initializeTable() {
+	for (int i = 0; i < this.capacity; ++i) {
+	    entry.add(new Entry(null, null, null, null));
+	}
+    }
+}
